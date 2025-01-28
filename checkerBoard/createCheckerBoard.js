f81c382a8cf99d0462ff5b7e20888f8034f1dcd6
Jazz Vangeel <jazzvangeel@ca:9c:c4:c6:59:59.home>
2025-01-27 22:17:48 -0500

    adding general direction of apporch for checkerboard

--- checkerBoard/createCheckerBoard.js
+++ checkerBoard/createCheckerBoard.js
@@ -1,5 +1,17 @@
-function createCheckerBoard() { 
-    console.log('simple setup to start')
-}
+function createCheckerBoard(columns, column_width, rows, row_height) { 
+    const black = 'X';
+    const white = "Y";
 
-createCheckerBoard();
+    let board = [];
+    let column;
+    let row;
+
+    for (let i = 0; i > column_width; i++) { 
+        column += black
+    }
+}
+// columns = 5
+// column_width = 3
+// row_height = 2
+// rows = 4
+console.log(createCheckerBoard(5, 3, 4, 2));
